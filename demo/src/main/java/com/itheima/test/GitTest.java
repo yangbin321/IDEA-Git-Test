c5b940210a4099a7854ca7908c7675aa787de0a0
yangbin321 <1010545743@qq.com>
2020-03-08 13:30:43 +0800

    向git本地仓库添加demo项目，添加了GitTtest测试方法

---
+++ demo/src/main/java/com/itheima/test/GitTest.java
@@ -0,0 +1,21 @@
+package com.itheima.test;
+
+/**
+ * @author Yangbin
+ * @create 2020-03-08 13:29
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+        System.out.println("hello git........... ");
+    }
+
+}
